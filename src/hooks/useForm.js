d6b15a3d3e9382c58c22f2f9577595e2de43d478
Role4Initiative <brusinowski@yahoo.com>
2022-01-14 17:51:33 -0500

    Completed tasks in PlantList.js and useForm.js built, working on CheckoutForm.js and interview questions next.

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,18 @@
 // write your custom hook here to control your checkout form
+
+import { useState } from "react";
+
+
+const useForm = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+    
+    const handleChanges = evt => {
+        setValue({
+            ...value, [evt.target.name]: evt.target.value
+        })
+    }
+
+    return [value, handleChanges];
+}
+
+export default useForm;
